53fe7d05685350155fc1789a1fa0bccc11602eed
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2023-09-16 16:18:10 +0700

    Method(BE): Learn Method for BE

--- test-da/method.py
+++ test-da/method.py
@@ -1,9 +1,8 @@
-name = input("Insert your name : ")
-age = int(input("Insert your age : "))
+def account(name):
+    username = name
+    return username
 
 
-def say_hello():
-    print(f"Hallo {name}, {age} years old")
+acc = account("xaxaxa")
 
-
-say_hello()
+print(f"Selamat! Akun {acc} anda berhasil dibuat")
