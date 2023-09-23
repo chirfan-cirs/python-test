1c83bf1486593419355d7261eef3cba8ff351716
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2023-09-23 16:27:50 +0700

    Dict(BE): Learn Dictionary and JSON

---
+++ test-da/dict.py
@@ -0,0 +1,28 @@
+users = {
+    "user1": {
+        "id": 1,
+        "name": "Xaxaxa",
+        "email": "Xaxaxa@gmail.com",
+    },
+    "user2": {
+        "id": 2,
+        "name": "asdf",
+        "email": "asdf@gmail.com"
+    }
+}
+
+print(users)
+print(users["user1"])
+print(users["user2"])
+print(users["user1"]["id"])
+print(type(users))
+
+print("\n To String")
+import json
+result = json.dumps(users)
+print(result)
+print(type(result))
+
+print("\n To file")
+with open('result.json', 'w') as file:
+    json.dump(users, file)
