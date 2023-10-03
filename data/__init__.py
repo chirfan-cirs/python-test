1ec12ed0ab85735f3b0f32474ebdb2eb506e7ad8
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2023-10-03 15:10:40 +0700

    Scrap The Most Read News Data from Kompas.com

--- data/__init__.py
+++ data/__init__.py
@@ -4,92 +4,51 @@ from bs4 import BeautifulSoup
 
 def extract_data():
     try:
-        link = requests.get('https://www.bmkg.go.id/')
-        # print(link.status_code)
+        link = requests.get('https://www.kompas.com/')
+        print(link.status_code)
     except Exception:
         return None
 
     if link.status_code == 200:
         src = BeautifulSoup(link.text, 'html.parser')
-        temp = src.find('div', {'class': 'prakicu-kota-besar-bg'})
-        h2_element = temp.findChildren('h2', {'class': 'kota'})
-        p_element = temp.findChildren('p')
+        result = src.find('div', {'class': 'most__wrap clearfix'})
+        result = result.findChildren('h4')
 
         i = 1
-        j = 1
-        kota = None
-        # waktu = None
-        # magnitude = None
-        # kedalaman = None
-        # koordinat = None
-        # lokasi = None
-        # dirasakan = None
+        most_title1 = None
+        most_title2 = None
+        most_title3 = None
+        most_title4 = None
+        most_title5 = None
 
-        for tem in h2_element:
-            print(i, tem)
-            if i == len(h2_element):
-                kota = tem.text
-            # if i == 1:
-            #     waktu = tem.text.split(', ')
-            #     tanggal = waktu[0]
-            #     jam = waktu[1]
-            # elif i == 2:
-            #     magnitude = tem.text
-            # elif i == 3:
-            #     kedalaman = tem.text
-            # elif i == 4:
-            #     koordinat = tem.text.split(' - ')
-            #     ls = koordinat[0]
-            #     bt = koordinat[1]
-            # elif i == 5:
-            #     lokasi = tem.text
-            # elif i == 6:
-            #     dirasakan = tem.text
+        for res in result:
+            # print(i, res)
+            if i == 1:
+                most_title1 = res.text
+            elif i == 2:
+                most_title2 = res.text
+            elif i == 3:
+                most_title3 = res.text
+            elif i == 4:
+                most_title4 = res.text
+            elif i == 5:
+                most_title5 = res.text
             i = i + 1
 
-        for temp in p_element:
-            # print(j, temp)
-            # if i == 1:
-            #     waktu = tem.text.split(', ')
-            #     tanggal = waktu[0]
-            #     jam = waktu[1]
-            # elif i == 2:
-            #     magnitude = tem.text
-            # elif i == 3:
-            #     kedalaman = tem.text
-            # elif i == 4:
-            #     koordinat = tem.text.split(' - ')
-            #     ls = koordinat[0]
-            #     bt = koordinat[1]
-            # elif i == 5:
-            #     lokasi = tem.text
-            # elif i == 6:
-            #     dirasakan = tem.text
-            j = j + 1
-
-    data = dict()
-    data['kota'] = kota
-    # data['tanggal'] = tanggal
-    # data['jam'] = jam
-    # data['magnitude'] = magnitude
-    # data['kedalaman'] = kedalaman
-    # data['ls'] = ls
-    # data['bt'] = bt
-    # data['lokasi'] = lokasi
-    # data['dirasakan'] = dirasakan
-    return data
+        data = dict()
+        data['one'] = most_title1
+        data['two'] = most_title2
+        data['three'] = most_title3
+        data['four'] = most_title4
+        data['five'] = most_title5
+        return data
 
 
 def view_data(result):
-    print(result)
-    for res in result['kota']:
-        print(f"Kota = {res}")
-
-    # print("\nGEMPA BUMI TERKINI")
-    # print(f"Tanggal : {result['tanggal']}")
-    # print(f"Jam : {result['jam']}")
-    # print(f"Magnitude : {result['magnitude']}")
-    # print(f"Kedalaman : {result['kedalaman']}")
-    # print(f"Kordinato : LS =  {result['ls']} BT = {result['bt']}")
-    # print(f"Lokasi : {result['lokasi']}")
-    # print(f"Dirasakan : {result['dirasakan']}")
+    # print(result)
+    print("THE MOST READ NEWS FROM Kompas.com")
+    print(f"Most Read News Number 1 : {result['one']}")
+    print(f"Most Read News Number 2 : {result['two']}")
+    print(f"Most Read News Number 3 : {result['three']}")
+    print(f"Most Read News Number 4 : {result['four']}")
+    print(f"Most Read News Number 5 : {result['five']}")
