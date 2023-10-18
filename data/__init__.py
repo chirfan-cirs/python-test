c9db7398ea9a279c62fe56ada469ad776db90ec3
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2023-10-18 14:50:44 +0700

    Extracting data from Kompas.com and save the data into CSV File

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 def extract_data():
     try:
         link = requests.get('https://www.kompas.com/')
@@ -15,47 +14,107 @@ def extract_data():
         result = result.findChildren('h4')
 
         i = 1
-        most_title1 = None
-        most_title2 = None
-        most_title3 = None
-        most_title4 = None
-        most_title5 = None
+        media1 = None
+        media2 = None
+        media3 = None
+        media4 = None
+        media5 = None
 
         for res in result:
-            # print(i, res)
             if i == 1:
-                most_title1 = res.text
+                media1 = res.text
             elif i == 2:
-                most_title2 = res.text
+                media2 = res.text
             elif i == 3:
-                most_title3 = res.text
+                media3 = res.text
             elif i == 4:
-                most_title4 = res.text
+                media4 = res.text
             elif i == 5:
-                most_title5 = res.text
+                media5 = res.text
             i = i + 1
 
         data = dict()
-        data['one'] = most_title1
-        data['two'] = most_title2
-        data['three'] = most_title3
-        data['four'] = most_title4
-        data['five'] = most_title5
+        data['media1'] = media1
+        data['media2'] = media2
+        data['media3'] = media3
+        data['media4'] = media4
+        data['media5'] = media5
         return data
 
-
 def view_data(result):
-    # print(result)
-    print("THE MOST READ NEWS FROM Kompas.com")
+    print("Data Scraping Kompas.com")
+    dat = result['media1'], result['media2'], result['media3'], result['media4'], result['media5']
+    index = range(1, 6)
+    for x, y in zip(index, dat):
+        print(f"Data berita Populer No {x}. {y}")
 
 
-    i = range(1, 6)
-    res = result['one'], result['two'], result['three'], result['four'], result['five']
-    for x, y in zip(i, res):
-        print(f"Most Read News Number {x} : {y}")
 
-    # print(f"Most Read News Number 1 : {result['one']}")
-    # print(f"Most Read News Number 2 : {result['two']}")
-    # print(f"Most Read News Number 3 : {result['three']}")
-    # print(f"Most Read News Number 4 : {result['four']}")
-    # print(f"Most Read News Number 5 : {result['five']}")
+
+
+
+
+
+
+
+# import requests
+# from bs4 import BeautifulSoup
+#
+#
+# def extract_data():
+#     try:
+#         link = requests.get('https://www.kompas.com/')
+#         print(link.status_code)
+#     except Exception:
+#         return None
+#
+#     if link.status_code == 200:
+#         src = BeautifulSoup(link.text, 'html.parser')
+#         result = src.find('div', {'class': 'most__wrap clearfix'})
+#         result = result.findChildren('h4')
+#
+#         i = 1
+#         most_title1 = None
+#         most_title2 = None
+#         most_title3 = None
+#         most_title4 = None
+#         most_title5 = None
+#
+#         for res in result:
+#             # print(i, res)
+#             if i == 1:
+#                 most_title1 = res.text
+#             elif i == 2:
+#                 most_title2 = res.text
+#             elif i == 3:
+#                 most_title3 = res.text
+#             elif i == 4:
+#                 most_title4 = res.text
+#             elif i == 5:
+#                 most_title5 = res.text
+#             i = i + 1
+#
+#         data = dict()
+#         data['one'] = most_title1
+#         data['two'] = most_title2
+#         data['three'] = most_title3
+#         data['four'] = most_title4
+#         data['five'] = most_title5
+#         return data
+#
+#
+# def view_data(result):
+#     print(result)
+#     print("THE MOST READ NEWS FROM Kompas.com")
+#
+#
+#     i = range(1, 6)
+#     res = result['one'], result['two'], result['three'], result['four'], result['five']
+#     for x, y in zip(i, res):
+#         print(f"Most Read News Number {x} : {y}")
+#
+#     print(f"Most Read News Number 1 : {result['one']}")
+#     # print(f"Most Read News Number 2 : {result['two']}")
+#     # print(f"Most Read News Number 3 : {result['three']}")
+#     # print(f"Most Read News Number 4 : {result['four']}")
+#     # print(f"Most Read News Number 5 : {result['five']}")
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-import data
+import csv
+import requests
+from bs4 import BeautifulSoup
 
-if __name__ == '__main__':
-    print("Main Application")
-    result = data.extract_data()
-    data.view_data(result)
+link = requests.get('https://www.kompas.com/')
+
+if link.status_code == 200:
+    src = BeautifulSoup(link.text, 'html.parser')
+    result = src.find('div', {'class': 'most__wrap clearfix'})
+    result = result.findChildren('h4')
+    data = []
+
+    i = 1
+
+    for news in result:
+        d = {'news number': f'Title {i}', 'news': news.text}
+        i += 1
+        data.append(d)
+
+    filename = 'kompas-scrap.csv'
+    with open(filename, 'wt+', newline='') as f:
+        w = csv.DictWriter(f, ['news number', 'news'])
+        w.writeheader()
+
+        for row in data:
+            w.writerow(row)
