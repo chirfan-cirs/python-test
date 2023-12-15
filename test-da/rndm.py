41d8e6e6d0fd517e5abb16c61ba20f876b467978
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2023-12-15 15:44:00 +0700

    just random code to using library time and random to help dictionary.

--- test-da/rndm.py
+++ test-da/rndm.py
@@ -1,23 +1,16 @@
-import time
-import random
 
-news = {
-    'title': 'Xaxaxxaxaxaxa',
-    'date': time.ctime(),
-    'view': random.seed(1)
-},{
-    'title': 'aasdasdwfwffw',
-    'date': time.ctime(),
-    'view': random.randint(1, 100)
-},{
-    'title': 'gdgafawfawfaw',
-    'date': time.ctime(10.0),
-    'view': random.randint(100, 1000)
+
+datas = {
+    "name" : "asdasd",
+    "age" : 14,
+}, {
+    "name" : "xaxaxa",
+    "age" : 72,
+}, {
+    "name" : "grwegf",
+    "age" : 42,
 }
 
-for res, i in zip(news, range(1,5)):
-    print("========================")
-    print(f"Berita ke-{i}")
-    print(f"Judul Berita            : {res['title']}")
-    print(f"Waktu Berita diposting  : {res['date']}")
-    print(f"Jumlah pembaca          : {res['view']}")
+print(datas)
+for data in datas:
+    print(data)
